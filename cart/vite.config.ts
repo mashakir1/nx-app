1d5b56ded19bc7158e4e502b74626c2273b4e223
mashakir1 <muhammad.shakir@nimbleapproach.com>
2022-12-09 17:21:14 +0000

    init commit

--- cart/vite.config.ts
+++ cart/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths({
-      root: './',
+      root: '../',
       projects: ['tsconfig.base.json'],
     }),
   ],
@@ -19,7 +19,7 @@ export default defineConfig({
   test: {
     globals: true,
     cache: {
-      dir: './node_modules/.vitest',
+      dir: '../node_modules/.vitest',
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
